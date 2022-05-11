e9dbb80df103588834b770556481b57dca60e952
varo <varomanukyan@gmail.com>
2022-05-11 17:50:42 +0400

    update db configs

--- src/DB/db.config.ts
+++ src/DB/db.config.ts
@@ -1,7 +1,7 @@
 export class DbConfig {
     public readonly database = 'testDB';
-    public readonly username = 'general-user';
-    public readonly password = 'fuckedlife';
+    public readonly username = 'postgres';
+    public readonly password = 'varofromdataart';
     public readonly host = 'localhost';
     public readonly dialect = 'postgres';
 }
