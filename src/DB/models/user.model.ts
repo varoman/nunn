1d94db701541aa2e0bd68a65407d042bcafa56c4
Varazdat Manukyan <varazdat.manukyan@click2sure.co.za>
2022-05-16 17:45:48 +0400

    hash password on user create

--- src/DB/models/user.model.ts
+++ src/DB/models/user.model.ts
@@ -2,14 +2,21 @@ import {
 	Model,
 	InferAttributes,
 	InferCreationAttributes,
-	CreationOptional, DataTypes,
+	CreationOptional,
+	DataTypes,
 } from 'sequelize';
 import database from '../database.js';
+import { HookReturn } from 'sequelize/types/hooks';
+import bcryptjs from 'bcryptjs';
+
+
 export class User extends Model <InferAttributes<User>, InferCreationAttributes<User>> {
 	declare id: CreationOptional<number>;
 	declare email: string;
 	declare name: string;
 	declare password: string;
+	declare createdAt: CreationOptional<Date>;
+	declare updatedAt: CreationOptional<Date>;
 }
 
 User.init({
@@ -30,9 +37,23 @@ User.init({
 		type: DataTypes.STRING,
 		allowNull: false
 	},
-}, {
-	tableName: 'users',
-	sequelize: database.getInstance(),
+	createdAt: {
+		type: DataTypes.DATE,
+		allowNull: false
+	},
+	updatedAt: {
+		type: DataTypes.DATE,
+		allowNull: false
+	}
+},
+	{
+		hooks: {
+			beforeCreate(user: User): HookReturn {
+				 user.password = bcryptjs.hashSync(user.password, 10);
+			}
+	},
+		tableName: 'users',
+		sequelize: database.getInstance(),
 });
 
 User.sync();
--- src/modules/auth/signup.ts
+++ src/modules/auth/signup.ts
@@ -5,8 +5,8 @@ import {User} from '../../db/models/user.model.js';
 export class Signup implements RequestHandler {
 	public async handleRequest(req: Request, res: Response, next?: NextFunction) {
 		const user: User = await User.create({
-			email: 'test email',
-			password: 'test password',
+			email: 'new email',
+			password: 'new password',
 			name: 'testname'
 		});
 		res.send('Signup');
