38a4db0a14bdcbd8955aff22356adc3637a56ae4
Varazdat Manukyan <varomanukyan@gmail.com>
2023-02-17 16:51:00 +0400

    Update db.config.ts

--- src/DB/db.config.ts
+++ src/DB/db.config.ts
@@ -3,7 +3,7 @@ import {Dialect} from "sequelize";
 export class DbConfig {
 	public readonly database: string = 'testDB';
 	public readonly username: string = 'postgres';
-	public readonly password: string = 'varofromdataart';
+	public readonly password: string = 'password';
 	public readonly host: string = '172.18.0.1';
 	public readonly dialect: Dialect = 'postgres';
 	public readonly port: number = 5433;
